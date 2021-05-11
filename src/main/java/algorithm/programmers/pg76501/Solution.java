aeaba477c7246117e3db5ba781809574f13f0a2d
unknown <ajrwk384@naver.com>
2021-05-11 22:08:14 +0900

    프로그래머스 '음양 더하기(76501)'

---
+++ src/main/java/algorithm/programmers/pg76501/Solution.java
@@ -0,0 +1,15 @@
+package algorithm.programmers.pg76501;
+
+public class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+                continue;
+            }
+            answer -= absolutes[i];
+        }
+        return answer;
+    }
+}
